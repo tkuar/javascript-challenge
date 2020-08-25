72b1a02ecd945d3439bbbf98470c7ac18021be8c
Tameka Kuar <tamekakuar17@gmail.com>
2020-08-25 11:28:39 -0400

    update app.js code

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,18 @@
 // from data.js
 var tableData = data;
+// Console.log the data from data.js
+// console.log(tableData);
 
-// YOUR CODE HERE!
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+tableData.forEach((data) => {
+    // Append a row for each object in tableData
+    var row = tbody.append("tr");
+
+    Object.entries(data).forEach(([key, value]) =>{
+        // Append a cell to the row for each value in the data object
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
